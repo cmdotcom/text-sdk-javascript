d5f01ea68392a58a1467d1307ea91b9959f4a9f8
Conor Shirren <32966008+ConorShirren@users.noreply.github.com>
2022-11-22 07:00:38 +0000

    Update MessageApiClient to support Telegram. (#41)
    Added channel `Telegram Messenger` as per the other text-sdk's

--- lib/MessageApiClient.ts
+++ lib/MessageApiClient.ts
@@ -1,7 +1,7 @@
 import * as CMTypes from "../typescript-node-client/api";
 import http = require('http');
 
-export type Channel = "SMS" | "Viber" | "RCS" | "Apple Business Chat" | "WhatsApp" | "Twitter" | "MobilePush" | "Facebook Messenger" | "Google Business Messages" | "Instagram";
+export type Channel = "SMS" | "Viber" | "RCS" | "Apple Business Chat" | "WhatsApp" | "Telegram Messenger" | "Twitter" | "MobilePush" | "Facebook Messenger" | "Google Business Messages" | "Instagram";
 export type RichMessage = CMTypes.RichMessage;
 export type Suggestion = CMTypes.Suggestion;
 export type Template = CMTypes.Template;
@@ -107,7 +107,7 @@ export class Message extends CMTypes.MessageEnvelope {
     /**
      * Sets the allowed channels to use. Default is to allow any channel configured for your account
      * @param channels array of allowed channels. 
-     * Any of "SMS", "Viber", "RCS", "Apple Business Chat", "WhatsApp" and "Twitter"
+     * Any of "SMS", "Viber", "RCS", "Apple Business Chat", "WhatsApp", "Telegram Messenger" and "Twitter"
      */
     public setAllowedChannels(channels: Channel[]): Message {
         this.messages.msg[0].allowedChannels = channels || [];
